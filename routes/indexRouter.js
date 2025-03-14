e7a42d29126418324004842f736979a7dff2da2b
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2025-03-15 00:49:21 +0200

    Add index Router

---
+++ routes/indexRouter.js
@@ -0,0 +1,38 @@
+const { Router } = require("express");
+const path = require("path");
+const { title } = require("process");
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
+const indexRouter = Router();
+indexRouter.get("/", (req, res) =>{
+    res.render("index", {
+        title:"Mini Messageboard",
+        messages: messages})
+});
+
+indexRouter.get("/new", (req, res) =>{
+    res.render("new")
+});
+
+indexRouter.post("/new", (req, res) =>{
+    messages.push({
+        text: req.body.messages,
+        user: req.body.name,
+        added: new Date()
+    })
+    res.redirect("/")
+})
+
+module.exports = indexRouter;
