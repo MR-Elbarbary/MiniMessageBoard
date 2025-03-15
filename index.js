55c270f3d6de4f8fa0eeff2c060c7143f8cff9de
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2025-03-15 02:04:37 +0200

    Set up Express server with view engine and routing

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const path = require("path");
+const indexRouter = require("./routes/indexRouter")
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const port = 8080
+app.listen(port, () => console.log("server is listening on port:" + port));
